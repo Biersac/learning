d06e18b73c419a5b4e10d035cca69d5ae342ac8a
John Watson <jwatson@flightstats.com>
2014-09-26 12:53:13 -0700

    added a few more fields

--- algorithms/src/main/java/com/flightstats/analytics/examples/Cars93Example.java
+++ algorithms/src/main/java/com/flightstats/analytics/examples/Cars93Example.java
@@ -17,28 +17,49 @@ import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toList;
 
+/**
+ * An example of training a simple regression tree. The tree doesn't get pruned, and it's grown as far as it possibly can.
+ * The data are from the R "cars93" dataset.
+ */
 public class Cars93Example {
+
+    public static final String MANUAL_AVAIL = "manual.avail";
+    public static final String HORSEPOWER = "horsepower";
+    public static final String WHEELBASE = "wheelbase";
+    public static final String DRIVETRAIN = "drivetrain";
+    public static final String TYPE = "type";
+
+    public static Map<String, Integer> typeMapping = new HashMap<>(5);
+
+    static {
+        typeMapping.put("\"Compact\"", 0);
+        typeMapping.put("\"Large\"", 1);
+        typeMapping.put("\"Midsize\"", 2);
+        typeMapping.put("\"Small\"", 3);
+        typeMapping.put("\"Sporty\"", 4);
+        typeMapping.put("\"Van\"", 5);
+    }
+
     public static void main(String[] args) throws IOException {
         Path dataDirectory = Paths.get("algorithms/testdata/cars93");
         Stream<String> lines = Files.lines(dataDirectory.resolve("Cars93.csv")).skip(1);
         List<LabeledMixedItem> data = lines.map(line -> {
             String[] fields = line.split(",");
             double price = Double.parseDouble(fields[5]);
-            double horsepower = Double.parseDouble(fields[13]);
-            double wheelbase = Double.parseDouble(fields[20]);
             Map<String, Double> continuousValues = new HashMap<>();
-            continuousValues.put("horsepower", horsepower);
-            continuousValues.put("wheelbase", wheelbase);
-
             Map<String, Integer> discreteValues = new HashMap<>();
-            discreteValues.put("manual", "\"Yes\"".equals(fields[16]) ? 1 : 0);
-            //0: Front, 1: Rear, 2: 4WD
-            discreteValues.put("drivetrain", "\"Front\"".equals(fields[10]) ? 0 : "\"Rear\"".equals(fields[10]) ? 1 : 2);
+
+            addDrivetrain(fields[10], discreteValues);
+            addHorsePower(fields[13], continuousValues);
+            addManualAvailable(fields[16], discreteValues);
+            addWheelBase(fields[20], continuousValues);
+            addType(fields[3], discreteValues);
+
             return new LabeledMixedItem(new MixedItem(fields[0], discreteValues, continuousValues), price);
         }).collect(toList());
 
         RegressionTreeTrainer trainer = new RegressionTreeTrainer();
-        RegressionTree tree = trainer.train("cars93", data, Arrays.asList("manual", "horsepower", "wheelbase", "drivetrain"));
+        RegressionTree tree = trainer.train("cars93", data, Arrays.asList(MANUAL_AVAIL, HORSEPOWER, WHEELBASE, DRIVETRAIN, TYPE));
 
         data.forEach(i -> {
             double result = tree.evaluate(i.getItem());
@@ -46,13 +67,39 @@ public class Cars93Example {
         });
 
         Map<String, Double> continuousValues = new HashMap<>();
-        continuousValues.put("horsepower", 80d);
-        continuousValues.put("wheelbase", 100d);
+        continuousValues.put(HORSEPOWER, 140d);
+        continuousValues.put(WHEELBASE, 100d);
+
         Map<String, Integer> discreteValues = new HashMap<>();
-        discreteValues.put("manual", 1);
-        discreteValues.put("drivetrain", 2);
+        discreteValues.put(MANUAL_AVAIL, 1);
+        discreteValues.put(DRIVETRAIN, 0);
+        discreteValues.put(TYPE, typeMapping.get("\"Sporty\""));
+
         MixedItem test = new MixedItem("test", discreteValues, continuousValues);
         double result = tree.evaluate(test);
         System.out.println("result = " + result);
+    }
+
+    private static void addManualAvailable(String field, Map<String, Integer> discreteValues) {
+        discreteValues.put(MANUAL_AVAIL, "\"Yes\"".equals(field) ? 1 : 0);
+    }
+
+    private static void addWheelBase(String field, Map<String, Double> continuousValues) {
+        double wheelbase = Double.parseDouble(field);
+        continuousValues.put(WHEELBASE, wheelbase);
+    }
+
+    private static void addHorsePower(String field, Map<String, Double> continuousValues) {
+        double horsepower = Double.parseDouble(field);
+        continuousValues.put(HORSEPOWER, horsepower);
+    }
+
+    private static void addDrivetrain(String field, Map<String, Integer> discreteValues) {
+        //0: Front, 1: Rear, 2: 4WD
+        discreteValues.put(DRIVETRAIN, "\"Front\"".equals(field) ? 0 : "\"Rear\"".equals(field) ? 1 : 2);
+    }
+
+    private static void addType(String field, Map<String, Integer> discreteValues) {
+        discreteValues.put(TYPE, typeMapping.get(field));
     }
 }
