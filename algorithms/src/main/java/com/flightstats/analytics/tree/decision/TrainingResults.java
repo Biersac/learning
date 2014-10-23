19fff40f06583a5fcd871b16f166b0b0acebb18b
John Watson <jwatson@flightstats.com>
2014-10-23 16:56:01 -0700

    streamlined the attribute importance code a tad.

--- algorithms/src/main/java/com/flightstats/analytics/tree/decision/TrainingResults.java
+++ algorithms/src/main/java/com/flightstats/analytics/tree/decision/TrainingResults.java
@@ -53,55 +53,29 @@ public class TrainingResults {
         return results;
     }
 
-    //todo: this is a lot of duplicated code...figure out how to unify it (and speed it up!).
+    //todo: speed it up!
     private Double calculateAttributeImportance(String attribute) {
-        if (attributeIsDiscrete(attribute)) {
-            double totalAcrossTrees = 0;
-            for (Map.Entry<Tree<Integer>, Set<LabeledItem<Integer>>> entry : outOfBagItemsByTree.entrySet()) {
-                Tree<Integer> tree = entry.getKey();
-                Set<LabeledItem<Integer>> outOfBagItems = entry.getValue();
-                List<Integer> possibleAttributeValues = outOfBagItems.stream().map(item -> item.getDiscreteValue(attribute)).collect(toList());
-                Collections.shuffle(possibleAttributeValues);
-                int numberCorrectMinusNumberRandomCorrect = 0;
-                for (LabeledItem<Integer> item : outOfBagItems) {
-                    Item randomizedItem = item.getItem().withDiscreteValue(attribute, possibleAttributeValues.remove(0));
-                    Integer realAnswer = tree.evaluate(item.getItem());
-                    Integer randomAnswer = tree.evaluate(randomizedItem);
-                    Integer labeledAnswer = item.getLabel();
-                    if (realAnswer.equals(labeledAnswer)) {
-                        numberCorrectMinusNumberRandomCorrect++;
-                    }
-                    if (randomAnswer.equals(labeledAnswer)) {
-                        numberCorrectMinusNumberRandomCorrect--;
-                    }
+        double totalAcrossTrees = 0;
+        for (Map.Entry<Tree<Integer>, Set<LabeledItem<Integer>>> entry : outOfBagItemsByTree.entrySet()) {
+            Tree<Integer> tree = entry.getKey();
+            Set<LabeledItem<Integer>> outOfBagItems = entry.getValue();
+            ItemRandomizer itemRandomizer = new ItemRandomizer(attribute, attributeIsDiscrete(attribute), outOfBagItems);
+            int numberCorrectMinusNumberRandomCorrect = 0;
+            for (LabeledItem<Integer> item : outOfBagItems) {
+                Item randomizedItem = itemRandomizer.randomize(item.getItem());
+                Integer realAnswer = tree.evaluate(item.getItem());
+                Integer randomAnswer = tree.evaluate(randomizedItem);
+                Integer labeledAnswer = item.getLabel();
+                if (realAnswer.equals(labeledAnswer)) {
+                    numberCorrectMinusNumberRandomCorrect++;
                 }
-                totalAcrossTrees += numberCorrectMinusNumberRandomCorrect;
-            }
-            return totalAcrossTrees / forest.size();
-        } else {
-            double totalAcrossTrees = 0;
-            for (Map.Entry<Tree<Integer>, Set<LabeledItem<Integer>>> entry : outOfBagItemsByTree.entrySet()) {
-                Tree<Integer> tree = entry.getKey();
-                Set<LabeledItem<Integer>> outOfBagItems = entry.getValue();
-                List<Double> possibleAttributeValues = outOfBagItems.stream().map(item -> item.getContinuousValue(attribute)).collect(toList());
-                Collections.shuffle(possibleAttributeValues);
-                int numberCorrectMinusNumberRandomCorrect = 0;
-                for (LabeledItem<Integer> item : outOfBagItems) {
-                    Item randomizedItem = item.getItem().withContinuousValue(attribute, possibleAttributeValues.remove(0));
-                    Integer realAnswer = tree.evaluate(item.getItem());
-                    Integer randomAnswer = tree.evaluate(randomizedItem);
-                    Integer labeledAnswer = item.getLabel();
-                    if (realAnswer.equals(labeledAnswer)) {
-                        numberCorrectMinusNumberRandomCorrect++;
-                    }
-                    if (randomAnswer.equals(labeledAnswer)) {
-                        numberCorrectMinusNumberRandomCorrect--;
-                    }
+                if (randomAnswer.equals(labeledAnswer)) {
+                    numberCorrectMinusNumberRandomCorrect--;
                 }
-                totalAcrossTrees += numberCorrectMinusNumberRandomCorrect;
             }
-            return totalAcrossTrees / forest.size();
+            totalAcrossTrees += numberCorrectMinusNumberRandomCorrect;
         }
+        return totalAcrossTrees / forest.size();
     }
 
     private boolean attributeIsDiscrete(String attribute) {
@@ -110,6 +84,33 @@ public class TrainingResults {
 
     public Matrix getItemProximities() {
         return itemProximities.divide(forest.size());
+    }
+
+
+    private static class ItemRandomizer {
+        private final List<?> possibleAttributes;
+        private final String attribute;
+        private final boolean isDiscrete;
+
+        public ItemRandomizer(String attribute, boolean isDiscrete, Set<LabeledItem<Integer>> outOfBagItems) {
+            this.attribute = attribute;
+            this.isDiscrete = isDiscrete;
+            if (!isDiscrete) {
+                this.possibleAttributes = outOfBagItems.stream().map(item -> item.getContinuousValue(attribute)).collect(toList());
+            } else {
+                this.possibleAttributes = outOfBagItems.stream().map(item -> item.getDiscreteValue(attribute)).collect(toList());
+            }
+            Collections.shuffle(possibleAttributes);
+        }
+
+        public Item randomize(Item item) {
+            if (isDiscrete) {
+                return item.withDiscreteValue(attribute, (Integer) possibleAttributes.remove(0));
+            } else {
+                return item.withContinuousValue(attribute, (Double) possibleAttributes.remove(0));
+            }
+        }
+
     }
 
 }
